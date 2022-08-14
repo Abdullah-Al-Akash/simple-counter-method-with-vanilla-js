1075db3522e1b4ddd0ed090d3baf4ef6becf6b66
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-08-14 23:25:32 +0600

    Using Redux

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-const incrementEl = document.getElementById('increment');
-const decrementEl = document.getElementById('decrement');
-const counterEl = document.getElementById('counter');
+// Select Elements:
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
 
-let count = 0;
+// Initialize State:
+const initialState = {
+        value: 0
+}
 
+// Create Reducer Function:
+function counterReducer(state = initialState, action) {
+        if (action.type === "increment") {
+                return {
+                        ...state,
+                        value: state.value + 1
+                }
+        }
+        else if (action.type === "decrement") {
+                return {
+                        ...state,
+                        value: state.value - 1
+                }
+        }
+        else {
+                return state;
+        }
+}
+
+// Create Store:
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+        const state = store.getState();
+        counterEl.innerText = state.value
+}
+
+store.subscribe(render)
+
+// Update UI Initially:
+render()
+
+// Add Event Listener:
 incrementEl.addEventListener('click', () => {
-        count++;
-        counterEl.innerText = count;
+        store.dispatch({
+                type: 'increment'
+        })
 })
 decrementEl.addEventListener('click', () => {
-        count--;
-        counterEl.innerText = count;
+        store.dispatch({
+                type: 'decrement'
+        })
 })
